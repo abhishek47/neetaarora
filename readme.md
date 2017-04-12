7391e7a3eb61c5dfcf1afcb4b6f48627cbf71fa3
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-12 11:32:35 +0530

    Ads

--- readme.md
+++ readme.md
@@ -50,3 +50,7 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 
 The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
 # neetaarora
+
+
+Sudo Password: PjAL0wI6e3QI28p8sqiE
+Database Password: eFFNUgh7UgbywQlvVzVp
--- resources/views/posts/sidebar.blade.php
+++ resources/views/posts/sidebar.blade.php
@@ -7,4 +7,30 @@
         <a href="/contact" class="button button-sm banner-button">Contact Us</a> </div>
     </div>
     
+     <div class="cell-sm-6 cell-md-12">
+	    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- Ad 1 -->
+		<ins class="adsbygoogle"
+		     style="display:block"
+		     data-ad-client="ca-pub-8327205924222954"
+		     data-ad-slot="7541066422"
+		     data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</div>
+	
+	<div class="cell-sm-6 cell-md-12">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- Ad 2 -->
+		<ins class="adsbygoogle"
+		     style="display:block"
+		     data-ad-client="ca-pub-8327205924222954"
+		     data-ad-slot="9017799627"
+		     data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>	
+   </div>
+    
   </div>
